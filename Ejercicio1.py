b733c242ae3a92a9648cc6050e3b497f5ecfac14
mininekos <manuel2g2m@gmail.com>
2022-11-23 10:32:39 +0100

    v1.1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -5,30 +5,27 @@ MAX_CUEVAS = 5
 puntos = 0
 perdido = False
 cont=0
+repetir=True
 
-print("*****************************************************************")
-print("*                    El reino del dragón                        *")
-print("*****************************************************************")
-time.sleep(1)
-def entrar():
-    print("Te acercas a una entrada y ves 2 cuevas\n"
-          "En una de ellas se encuetra un dragon amistoso y en otra un dragon malvado\n"
-          "¿En cual entras? 1 ó 2")
-    cuevaSeleccionada=input();
-    return cuevaSeleccionada
+while repetir == True:
+    while cont<MAX_CUEVAS and perdido==False:
+        print("Te acercas a una entrada y ves 2 cuevas\n"
+              "En una de ellas se encuetra un dragon amistoso y en otra un dragon malvado\n"
+              "¿En cual entras? 1 ó 2")
+        cueva = input()
 
-while cont<MAX_CUEVAS and perdido==False:
-
-    cueva=entrar()
-    dragonBueno = random.randint(1, 2)
-    print("Has entrado en la cueva de .....")
-    time.sleep(1)
-    if dragonBueno == int(cueva):
-        print("Un dragon bueno\nTe da su tesoro")
-        puntos += 100
+        dragonBueno = random.randint(1, 2)
+        print("Has entrado en la cueva de .....")
+        time.sleep(1)
+        if dragonBueno == int(cueva):
+            print("Un dragon bueno\nTe da su tesoro")
+            puntos += 100
+        else:
+            print("Un dragon malvado y te devora\n Has perdido\n")
+            perdido = True
+    print(f"Has obtenido {puntos} puntos")
+    if input("¿Desea volver a jugar? ").lower().__eq__("si"):
+        perdido = False
+        puntos = 0
     else:
-        print("Un dragon malvado y te devora")
-        perdido = True
-
-
-print(f"Has obtenido {puntos} puntos")
+        repetir=False
